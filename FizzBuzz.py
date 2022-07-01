0acd0749eb43c8c2f9cbaef8701fc29e5242537f
zebbyG <99161685+zebbyG@users.noreply.github.com>
2022-07-01 16:38:28 +0300

    FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,9 @@
-for num in range(1, 1000):
-    if num % 3 == 0 and num % 5 == 0:
+for number in range(1, 1000):
+    if number % 3 == 0 and number % 5 == 0:
       print("FizzBuzz")
-    elif num % 3 == 0:
+    elif number % 3 == 0:
       print("Fizz")
-    elif num % 5 == 0:
+    elif number % 5 == 0:
       print("Buzz")
     else:
-      print(num)
+      print(number)
