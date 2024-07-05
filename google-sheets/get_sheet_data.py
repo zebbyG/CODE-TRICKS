5a1a1b2f52468ff0777021f7ff7236356162d71f
Zebby <zebbynjau@gmail.com>
2024-07-05 20:42:04 +0300

    Update google sheets values fetch method

--- google-sheets/get_sheet_data.py
+++ google-sheets/get_sheet_data.py
@@ -1,6 +1,7 @@
 import json
 import os
-
+# Create <test_creds.json>
+# Login to Gcloud, create OAuth client, download the json file and update the values on <test_creds.json>
 from google.oauth2.credentials import Credentials # Gets the credentials from the cloud API
 from google_auth_oauthlib.flow import InstalledAppFlow # Creates/simplifies the flow of fetching credentials
 from googleapiclient.discovery import build # Builds a service object to be sent to the API for authorization
@@ -10,8 +11,8 @@ from googleapiclient.errors import HttpError # Logs the HTTP errors.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'] # Permission given to the google sheet.
 
 
-TEST_SURVEY_SPREADSHEET_ID = '1dBGAzEhxxS7inXP__6EpD7KDq6TxI2jHHfgmHEMDs68' # A unique ID to identify the sheet....[change it to your preferable sheet]
-TEST_SURVEY_RANGE = '1:1011' # The range of values accessed in the sheet. [match the range of the sheet id specified above]
+TEST_SURVEY_SPREADSHEET_ID = '1wXpGHCCICG5Q9NdGQimZC_bra8xfBm-VRru42h7iWH4' # A unique ID to identify the sheet....[change it to your preferable sheet]
+TEST_SURVEY_RANGE = '1:5107' # The range of values accessed in the sheet. [match the range of the sheet id specified above]
 
 
 def main():
@@ -33,6 +34,7 @@ def main():
         # After getting the creds, the build function creates a service object and sends it to the API,
         # <sheets> is what you are trying to access, <v4> is the version and the credentials to view the sheet
         service = build('sheets', 'v4', credentials=creds)
+
 
         sheet = service.spreadsheets()
         # After a successful connection to google sheet API, result variable values specifies what sheet is needed, and what range you want to access.
